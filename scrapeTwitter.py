91a6be4cf12777655006d8863572c01713330538
Steve Lee <smlee.981@gmail.com>
2018-06-16 17:24:23 -0500

    running loop to write to db by month

--- scrapeTwitter.py
+++ scrapeTwitter.py
@@ -11,25 +11,30 @@ log.basicConfig(level=log.INFO)
 
 username = config.username
 password = config.password
+dates = ["2017-08-01", "2017-09-01", "2017-10-01", "2017-11-01", "2017-12-01", "2018-01-01", "2018-02-01", "2018-03-01", "2018-04-01", "2018-05-01", "2018-06-01", "2018-07-01"]
 
 # set parameters
-search_query = "ethereum"
+search_query = "bitcoin"
 rate_delay_seconds = 0
 error_delay_seconds = 5
-select_tweets_since = datetime.datetime.strptime("2017-09-01", '%Y-%m-%d')
-select_tweets_until = datetime.datetime.strptime("2018-01-01", '%Y-%m-%d')
 threads = 1
 
-print("running search")
-# execute
-twitSlice = TwitterScraper.TwitterSlicer(rate_delay_seconds, error_delay_seconds, select_tweets_since, select_tweets_until, threads)
-twitSlice.search(search_query)
-twitter_df = twitSlice.df
+# pull dates at index i and i+1 to set range
+for i in range(len(dates) - 1):
+    select_tweets_since = datetime.datetime.strptime(dates[i], '%Y-%m-%d')
+    select_tweets_until = datetime.datetime.strptime(dates[i+1], '%Y-%m-%d')
 
-print("opening connection...")
-engine = create_engine('mysql+mysqlconnector://' + username + ':' + password + '@twitterdata.ckmmf3gk0i4d.us-east-2.rds.amazonaws.com:3306/tweets', echo=False)
+    # execute
+    print("running search for dates " + dates[i] + " - " + dates[i+1])
+    twitSlice = TwitterScraper.TwitterSlicer(rate_delay_seconds, error_delay_seconds, select_tweets_since, select_tweets_until, threads)
+    twitSlice.search(search_query)
+    twitter_df = twitSlice.df
 
-print("writing data...")
-twitter_df.to_sql(name="ethereum", con=engine, if_exists = 'append', index=False, chunksize=5000)
+    # write to mysql database
+    print("opening connection...")
+    engine = create_engine('mysql+mysqlconnector://' + username + ':' + password + '@twitterdata.ckmmf3gk0i4d.us-east-2.rds.amazonaws.com:3306/tweets', echo=False)
 
-print("wrote to db!")
+    print("writing data...")
+    twitter_df.to_sql(name="bitcoin", con=engine, if_exists = 'append', index=False, chunksize=5000)
+
+    print("wrote to db!")
