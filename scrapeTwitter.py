edb9bd25dff4105644c76dbf394a7b918c8d5de4
Steve Lee <smlee.981@gmail.com>
2018-07-09 16:51:22 -0500

    restored user-agent to original

--- scrapeTwitter.py
+++ scrapeTwitter.py
@@ -12,7 +12,7 @@ log.basicConfig(level=log.INFO)
 
 username = config.username
 password = config.password
-dates = ["2017-12-04", "2018-12-08"]
+dates = ["2017-12-08", "2018-12-12"]
 
 # set parameters
 search_query = "bitcoin"
